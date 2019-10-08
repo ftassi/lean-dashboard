d45bd0dfbc64fc674388de9ebf700bcde4663ab4
Francesco Tassi <tassi.francesco@gmail.com>
2019-10-09 01:28:55 +0200

    Updates

---
+++ precache-manifest.6d57fa042a395d364cf44d926cc8e25d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa751a3b2a106f8272f21dd6660c72ae",
+    "url": "/lean-dashboard/index.html"
+  },
+  {
+    "revision": "1ad5eb2689214667a605",
+    "url": "/lean-dashboard/static/css/main.3237dd5a.chunk.css"
+  },
+  {
+    "revision": "9ca6d25b494773390343",
+    "url": "/lean-dashboard/static/js/2.9a580d51.chunk.js"
+  },
+  {
+    "revision": "1ad5eb2689214667a605",
+    "url": "/lean-dashboard/static/js/main.1bacb7dd.chunk.js"
+  },
+  {
+    "revision": "20d49438ab949e25c87c",
+    "url": "/lean-dashboard/static/js/runtime-main.d6ec5981.js"
+  }
+]);
